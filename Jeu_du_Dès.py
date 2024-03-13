c592aef7d014c4d39abf81f4d3c1c25d60752b37
ThEo_BrNg <161337961+ThEbRnGGMS@users.noreply.github.com>
2024-03-13 15:20:16 +0100

    Upgrade 2 Jeu_du_Dès.py
    Merci à celui qui m'a aider à le finir !!!

--- Jeu_du_Dès.py
+++ Jeu_du_Dès.py
@@ -1,44 +1,113 @@
 import random
 import time
 
-print("Bienvenue dans le jeux du dès")
+print("Bienvenue sur le jeux du dès")
 
-nombre_partie = 0
+player1 = input("Nom du player 1: ")
+player2 = input("Nom du player 2: ")
 
-joueur1 = str(input("Nom du joueur 1 : "))
-joueur2 = str(input("Nom du joueur 2 : "))
+action_options = [
+    "La chose la plus embarrassante que j'aie faite en public :",
+     "Le plus gros mensonge jamais dit à tes parents:",
+     "La personne sur qui tu as actuellement un béguin secret:",
+     "La pire date:",
+     "Blague embarrassante sur quelqu'un:",
+     "Pire habitude à abandonner:",
+     "Avez-vous déjà triché à un examen ou un test ? Si oui, comment ?:",
+     "La chose la plus folle que vous ayez jamais faite en secret:",
+     "Secret le plus sombre:",
+     "Punition la pire que vous ayez jamais reçue:",
+     "Plus grand rêve non poursuivi: ",
+     "Personne la plus détestée dans la pièce et pourquoi ?: ",
+     "La chose la plus embarrassante que vous ayez jamais dite à quelqu'un:",
+     "La dernière chose que vous avez cherchée sur votre téléphone:",
+     "Avez-vous déjà volé quelque chose ? Si oui, quoi et pourquoi ?:",
+     "La pire chose que vous ayez jamais dite à quelqu'un lors d'une dispute :",
+]
 
-dice = str(input("Veut tu lancer le dès (o = Oui ; n = Non) ? "))
+truth_options = [
+    "Avez-vous déjà menti à votre meilleur ami ? Si oui, à propos de quoi ?",
+     "Quel est ton plus grand regret dans la vie jusqu'à présent ?",
+     "Avez-vous déjà eu le béguin pour un professeur ? Qui ?",
+     "Quel est ton plus grand secret que tu n'as jamais osé révéler à personne ?",
+     "Avez-vous déjà triché dans une relation amoureuse ? Si oui, comment ?",
+     "Quelle est votre plus grande peur irrationnelle ?",
+     "Quel est le pire mensonge que vous ayez jamais dit à vos parents ?",
+     "Avez-vous déjà voulu disparaître complètement de la vie de quelqu'un ? Pourquoi ?",
+     "Quelle est la chose la plus étrange que vous ayez jamais mangée ?",
+     "Selon vous, quelle est votre pire qualité ?",
+     "Avez-vous déjà regretté d'avoir commencé une relation amoureuse avec quelqu'un ? Pourquoi ?",
+     "Quelle est la pire chose que vous ayez jamais dite à quelqu'un et que vous regrettez maintenant ?"
+]
 
-option_action = ['Quelle est la chose la plus embarrassante que tu aies faite en public ?', 'Quel est le mensonge le plus gros que tu aies jamais dit à tes parents ?', 'Quelle est la personne que tu ascrush secrètement en ce moment ?', 'Décris ton pire rendez-vous amoureux', 'Raconte une blague embarrassante que tu as faite à quelqu un ?', 'Quelle est la pire habitude que tu aimerais abandonner ?', 'As-tu déjà triché à un examen ou un test, Si oui, comment ?', 'Quelle est la chose la plus folle que tu aies jamais faite en secret ?', 'Quel est ton plus grand regret dans la vie jusqu à présent ?', 'Quel est le secret le plus sombre que tu n as jamais partagé avec personne ?', 'Quelle est la pire punition que tu aies jamais reçue ?', 'Quel est ton plus grand rêve que tu n as jamais osé poursuivre ?', 'Quelle est la personne que tu détestes le plus dans cette pièce et pourquoi ?', 'Quelle est la chose la plus gênante que tu aies jamais dite à quelqu un ?', 'Quelle est la dernière chose que tu as recherchée sur ton téléphone ?', 'Si tu pouvais échanger ta vie avec quelqu un d autre pour une journée, qui choisirais-tu et pourquoi ?', 'Quelle est la chose la plus folle que tu aies jamais achetée en ligne ?', 'As-tu déjà volé quelque chose ? Si oui, quoi et pourquoi ?', 'Quel est ton plus grand secret honteux ?', 'Quelle est la pire chose que tu as dite à quelqu un lors d une dispute ?']
-option_verité = ['As-tu déjà menti à ton meilleur ami/amie ? Si oui, à propos de quoi ?', 'Quel est ton plus grand regret dans la vie jusqu à présent ?', 'As-tu déjà eu un coup de cœur pour un(e) professeur(e) ? Qui ?', 'Quel est ton plus grand secret que tu n as jamais osé révéler à personne ?', 'As-tu déjà triché dans une relation amoureuse ? Si oui, comment ?', 'Quelle est ta plus grande peur irrationnelle ?', 'Quel est le pire mensonge que tu aies jamais dit à tes parents ?', 'As-tu déjà eu envie de disparaître complètement de la vie de quelqu un ? Pourquoi ?', 'Quelle est la chose la plus bizarre que tu aies jamais mangée ?', 'Quel est ton pire défaut selon toi ?', 'As-tu déjà regretté de commencer une relation amoureuse avec quelqu un ? Pourquoi ?', 'Quelle est la chose la plus embarrassante que tu aies jamais faite en public ?', 'As-tu déjà eu des pensées violentes envers quelqu un ? Qui et pourquoi ?', 'Quelle est la chose la plus stupide que tu aies faite par amour ?', 'As-tu déjà eu des pensées de trahison envers un ami proche ? Pourquoi ?', 'Quelle est ta plus grande faiblesse en tant que personne ?', 'As-tu déjà eu des fantasmes inavouables ? Lesquels ?', 'Quelle est la chose que tu détestes le plus chez toi ?', 'As-tu déjà eu peur de perdre quelqu un de cher ? Qui et pourquoi ?', 'Quelle est la pire chose que tu aies jamais dite à quelqu un et que tu regrettes maintenant']  
-rejouer = 'o'
+replay = 'y'
 
-time.sleep(2)
+time.sleep(0)
 
-while rejouer != 'n':
-    
-    if nombre_partie%2 == 0:
-        print("Au tour de"" ""[",joueur1,"]")
+
+def roll_dice():
+    return random.randint(0, 6)
+
+
+def choose_question(options):
+    return random.choice(options)
+
+
+player1_answers = []
+player2_answers = []
+is_player1_turn = True
+
+game_duration = int(input("Combien de minutes veut-tu jouer ??? "))
+
+start_time = time.time()
+end_time = start_time + (60 * game_duration)
+
+while time.time() < end_time:
+    if is_player1_turn:
+        current_player = player1
+        other_player = player2
     else:
-        print("Au tour de"" ""[",joueur2,"]")
-        
+        current_player = player2
+        other_player = player1
+
+    print(f"C'est le tour de [{current_player}]")
     time.sleep(1)
-    
-    if dice == 'o':
-        resultat = random.randint(0, 6)
-        print(resultat)
 
-        if resultat%2 == 0:
-            qst_action = random.choices(option_action)
-            print(qst_action)
+    roll_dice_input = input(f"{current_player}, Veut tu faire rouler le dès? (o = oui; n = non): ")
 
-        elif resultat%2 == 1:
-            qst_verité = random.choices(option_verité)
-            print(qst_verité)
-            
-    time.sleep(5)
-    
-    nombre_partie += 1
-    
-    rejouer = str(input("Veut tu relancer le dès ? (o = oui ; n = non) ? "))
+    if roll_dice_input == 'o':
+        result = roll_dice()
+        print(result)
+
+        if result % 2 == 0:
+            action_question = choose_question(action_options)
+            print(action_question)
+            answer = input("Ta réponse: ")
+            player1_answers.append(answer) if is_player1_turn else player2_answers.append(answer)
+        else:
+            truth_question = choose_question(truth_options)
+            print(truth_question)
+            answer = input("Ta réponse: ")
+
+            player1_answers.append(answer) if is_player1_turn else player2_answers.append(answer)
+
+    time.sleep(0)
+
+    if len(player1_answers) == len(player2_answers):
+        replay = input("Veut tu relancer le dès? (o = oui; n = non): ")
+    else:
+        replay = 'n'
+        print("Tourne ton tour. Au joueur suivant")
+
+    is_player1_turn = not is_player1_turn
+
+print("\nFin du jeux!")
+print(f"Les questions de {player1}")
+for i, answer in enumerate(player1_answers):
+    print(f"Question {i + 1}: {answer}")
+
+print("\n")
+
+print(f"Les question de {player2}:")
+for i, answer in enumerate(player2_answers):
+    print(f"Question {i + 1}: {answer}")
